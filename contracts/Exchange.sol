db8781b31b29fffd51d158a11627b5913a21ca59
Darwin Subramaniam <darwinsubramaniam@googlemail.com>
2022-07-24 19:06:22 +0800

    exchange contract deposit and balanceof

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,23 +1,47 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-import "hardhat/console.sol";
 
-// [] Deposit Token 
+import "hardhat/console.sol";
+import "./Token.sol";
+
+// [x] Deposit Token
 // [] Withdraw Tokens
-// [] Check Balances
+// [x] Check Balances
 // [] Make Orders
 // [] Cancel Orders
 // [] Fill Orders
-// [] Charge Fees 
-// [x] Track Fee Account 
+// [] Charge Fees
+// [x] Track Fee Account
 contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
 
-    constructor(
-        address _feeAccount,
-        uint256 _feePercent){
+    // token address => owner address => total deposited
+    mapping(address => mapping(address => uint256)) public tokens;
+
+    event Deposit(address token, address user, uint256 ammount , uint256 balance);
+
+    constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
+    }
+
+    // Deposit and withdraw Token
+    function depositToken(address _token, uint256 _amount) public {
+        // transfer token to exchange
+        require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+        // update user balance
+        tokens[_token][msg.sender] += _amount;
+        // Emit an event
+        emit Deposit(_token,msg.sender,_amount,tokens[_token][msg.sender]);
+    }
+
+    // Check balances
+    function balanceOf(address _token, address _user)
+        public
+        view
+        returns (uint256)
+    {
+        return tokens[_token][_user];
     }
 }
--- test/Exchange.ts
+++ test/Exchange.ts
@@ -1,6 +1,6 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
-import { BigNumber, ContractReceipt, ContractTransaction } from "ethers";
+import { Contract, ContractReceipt, ContractTransaction } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Token } from "../typechain-types";
 import { WeiHelper } from "./Helper/WeiParser";
@@ -9,26 +9,43 @@ describe("Exchange", () => {
   let deployer: SignerWithAddress,
     accounts: SignerWithAddress[],
     feeAccount: SignerWithAddress,
-    exhange: any;
+    exchange: Contract;
 
-    const feePercent = 10;
+  let token_one: Token,
+    token_two: Token;
+  let myWallet: SignerWithAddress,
+    otherPersonWallet: SignerWithAddress;
+
+  const feePercent = 10;
 
   beforeEach(async () => {
     accounts = await ethers.getSigners();
     deployer = accounts[0];
     feeAccount = accounts[1];
+    myWallet = accounts[2];
+    otherPersonWallet = accounts[3];
 
-    const Exchange = await ethers.getContractFactory('Exchange');
-    exhange = await Exchange.deploy(feeAccount.address, feePercent);
+    const Exchange = await ethers.getContractFactory("Exchange");
+    const Token = await ethers.getContractFactory("Token");
+
+    exchange = await Exchange.connect(myWallet).deploy(
+      feeAccount.address,
+      feePercent,
+    );
+    token_one = await Token.connect(myWallet).deploy(
+      "1Token",
+      "ONE",
+      WeiHelper.parse(100_000),
+    );
   });
 
   describe("Deployment", () => {
     it("track the fee account", async () => {
-        expect(await exhange.feeAccount()).to.equal(feeAccount.address);
+      expect(await exchange.feeAccount()).to.equal(feeAccount.address);
     });
 
     it("track the fee percent", async () => {
-        expect(await exhange.feePercent()).to.be.equal(10);
+      expect(await exchange.feePercent()).to.be.equal(10);
     });
 
     it("able to withdraw correct ammount", async () => {
@@ -47,6 +64,70 @@ describe("Exchange", () => {
     });
 
     it("when order filled , charge are place", async () => {
+    });
+  });
+
+  describe("Depositing Tokens", () => {
+    let transaction: ContractTransaction;
+    let result: ContractReceipt;
+    let ammount = WeiHelper.parse(100);
+    beforeEach(async () => {
+      // Approve Token
+      let approvalTx = await token_one.connect(myWallet).approve(
+        exchange.address,
+        ammount,
+      );
+      await approvalTx.wait();
+      // Deposit Token
+      transaction = await exchange.connect(myWallet).depositToken(
+        token_one.address,
+        ammount,
+      );
+      result = await transaction.wait();
+    });
+
+    describe("Success", () => {
+      it("tracks the token deposit", async () => {
+        expect(await token_one.balanceOf(exchange.address)).to.equal(ammount);
+        expect(await exchange.tokens(token_one.address, myWallet.address)).to.be
+          .equal(ammount);
+        expect(await exchange.balanceOf(token_one.address, myWallet.address)).to
+          .be.equal(ammount);
+      });
+      it("emit Deposit event", () => {
+        const event = result.events![1];
+        expect(event.event).to.equal("Deposit");
+        const args = event.args!;
+        expect(args.token).to.equal(token_one.address);
+        expect(args.user).to.equal(myWallet.address);
+        expect(args.ammount).to.equal(ammount);
+        expect(args.balance).to.equal(ammount);
+      });
+    });
+
+    describe("Failure", () => {
+      it("reverted: token not approved", async () => {
+        await expect(
+          exchange.connect(myWallet)
+            .depositToken(token_one.address, ammount),
+        )
+          .to.be.reverted;
+      });
+
+      it("should fail if the user approved ammount is let than attempt to transfer", async () => {
+        let approvedAmmount = WeiHelper.parse(100);
+        let invalidAmmount = WeiHelper.parse(101);
+        let approvaltx = await token_one.connect(myWallet)
+          .approve(exchange.address, approvedAmmount);
+        await approvaltx.wait();
+
+        await expect(
+          exchange.connect(myWallet).depositToken(
+            token_one.address,
+            invalidAmmount,
+          ),
+        ).to.be.rejected;
+      });
     });
   });
 });
